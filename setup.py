c8e155c6db50a1fcb6c996dbcca7a8c5a2aaaabd
ri0t <riot@c-base.org>
2019-01-22 22:24:00 +0100

    Fixed isomer version requirement

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ This software package is a plugin module for Isomer
 """,
       dependency_links=[],
       install_requires=[
-          'isomer>=1.2.0'
+          'isomer>=1.0.0'
       ],
       entry_points="""[isomer.components]
     [isomer.schemata]
