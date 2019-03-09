56182edfed9839fa52354949bf917c5548b3597f
ri0t <riot@c-base.org>
2019-03-09 18:29:48 +0100

    Fixed packaging, added Trove classifiers

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 # HFOS - Hackerfleet Operating System
 # ===================================
-# Copyright (C) 2011-2018 Heiko 'riot' Weinen <riot@c-base.org> and others.
+# Copyright (C) 2011-2019 Heiko 'riot' Weinen <riot@c-base.org> and others.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -23,27 +23,60 @@ __license__ = "AGPLv3"
 
 from setuptools import setup, find_packages
 
-setup(name="hfos",
-      version="1.2.1",
-      description="hfos - an operating system for vessels",
-      author="Hackerfleet Community",
-      author_email="riot@c-base.org",
-      url="https://github.com/Hackerfleet/hfos",
-      license="GNU Affero General Public License v3",
-      packages=find_packages(),
-      long_description="""HFOS
+setup(
+    name="hfos",
+    version="1.2.1",
+    description="hfos - an operating system for vessels",
+    author="Hackerfleet Community",
+    author_email="riot@c-base.org",
+    url="https://github.com/Hackerfleet/hfos",
+    license="GNU Affero General Public License v3",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Isomer :: 1',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Operating System :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    long_description="""HFOS
 ====
 
 A module to bundle Hackerfleet's Isomer modules to a ship computer operating system.
 
 This software package is a plugin module for Isomer
 """,
-      dependency_links=[],
-      install_requires=[
-          'isomer>=1.0.0'
-      ],
-      entry_points="""[isomer.components]
+    dependency_links=[],
+    install_requires=[
+        'isomer',
+        'hfos-alert',
+        'hfos-anchor',
+        'hfos-busrepeater',
+        'hfos-comms',
+        'hfos-crew',
+        'hfos-dashboard',
+        'hfos-dev',
+        'hfos-equipment',
+        'hfos-logbook',
+        'hfos-map',
+        # 'hfos-mesh',
+        'hfos-navdata',
+        'hfos-nmea',
+        'hfos-nodestate',
+        'hfos-webguides'
+    ],
+    entry_points="""[isomer.components]
+    hfoscore=isomer.hfos.core:HFOSCore
     [isomer.schemata]
     """,
-      test_suite="tests.main.main",
-      )
+    test_suite="tests.main.main",
+)
